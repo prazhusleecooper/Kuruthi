555242441b049054c693b439b8e4dee9b3f091eb
prazhusleecooper <prashanth5454@gmail.com>
2020-02-18 06:44:46 -0800

    sign-in and sign-up pages

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import "../Resources/CSS/Signin.scss"
+import '../Resources/CSS/Signin.scss';
 
 class Signin extends Component {
     constructor(props) {
@@ -12,10 +12,19 @@ class Signin extends Component {
             <div>
                 {/* Large screens */}
                 <div className="sign-in-lg">
-                    <div className="d-flex flex-column align-items-center justify-content-center primary-area">
-                        <div className="heading-section">
-                            Sign in
-                            <hr />
+                    <div className="d-flex flex-column align-items-center justify-content-center primary-area-section">
+                        <div className="d-flex flex-column align-items-start justify-content-center primary-area">
+                            <div className="heading-section">
+                                Sign in
+                                <hr className="primary-area-hr"/>
+                            </div>
+                            <label className="text-input-label">Email</label>
+                            <input type="text" className="emailInput" placeholder="Eg. you@yourmail.com" required={true}/>
+                            <label className="text-input-label">Password</label>
+                            <input type="text" className="passwordInput" placeholder="Password" required={true}/>
+                        </div>
+                        <div className="submit-btn-section">
+                            <button className="submit-btn">Sign in</button>
                         </div>
                     </div>
                 </div>
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import '../Resources/CSS/Signup.scss'
 
 class Signup extends Component {
     constructor(props) {
@@ -8,7 +9,33 @@ class Signup extends Component {
 
     render() {
         return(
-            <div>Sign UP</div>
+            <div>
+                {/* Large screens */}
+                <div className="sign-up-lg">
+                    <div className="d-flex flex-column align-items-center justify-content-center primary-area-section">
+                        <div className="d-flex flex-column align-items-start justify-content-center primary-area">
+                            <div className="heading-section">
+                                Sign up
+                                <hr className="primary-area-hr"/>
+                            </div>
+                            <label className="text-input-label">Email</label>
+                            <input type="text" className="emailInput" placeholder="Eg. you@yourmail.com" required={true}/>
+                            <label className="text-input-label">Password</label>
+                            <input type="text" className="passwordInput" placeholder="Password" required={true}/>
+                            <label className="text-input-label">Re-type Password</label>
+                            <input type="text" className="passConfirmInput" placeholder="Re-type Password" required={true}/>
+                        </div>
+                        <div className="tc-text-section">
+                            <span className="tc-text">
+                                By signing up you agree to our Terms & conditions
+                            </span>
+                        </div>
+                        <div className="submit-btn-section">
+                            <button className="submit-btn">Sign up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     };
 }
